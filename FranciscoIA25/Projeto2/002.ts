2dbf80e88cee4cc779c3c6e20b0a39dd9c2fbc7b
Francisco <frangeissmann@gmail.com>
2026-03-26 01:41:01 +0000

    Atualização com os códigos dos 3 tipos de triângulos

--- FranciscoIA25/Projeto2/002.ts
+++ FranciscoIA25/Projeto2/002.ts
@@ -1,16 +1,33 @@
-class Triangulo {
-    base: number;
-    altura: number;
+class TrianguloEquilatero {
+    ladosTriangulo: number;
 
-    calcularArea(): number {
-        return (this.base * this.altura) / 2;
+    calcArea(): number {
+        return this.ladosTriangulo ** 2 * Math.sqrt(3) / 4 
     }
-    constructor(pBase: number, pAltura: number){
-        this.base = pBase
-        this.altura = pAltura
+    constructor(pLadoTriangulo: number){
+        this.ladosTriangulo = pLadoTriangulo
+
     }
 }
+class TrianguloIsoceles {
+    calcArea(): number {
+        return  this.base * this.altura / 2
+    }
+    constructor(public base: number, public altura: number) {}
+}
 
-const tri1 = new Triangulo( 10, 5);
+class TrianguloEscaleno {
+    calcArea():number {
+        let p: number = (this.lado1+this.lado2+this.lado3) / 2
+        return Math.sqrt(p*(p-this.lado1)*(p-this.lado2)*(p-this.lado3))
+    }
+    constructor(public lado1:number, public lado2:number, public lado3: number) {}
+}
 
-console.log(tri1.calcularArea());
+const tri1 = new TrianguloEquilatero(10);
+const tri2 = new TrianguloIsoceles(10, 5)
+const tri3 = new TrianguloEscaleno(5, 6, 7)
+
+console.log(`Área do triângulo Equilátero ${tri1.calcArea()}`);
+console.log(`Área do triângulo Isóceles ${tri2.calcArea()}`);
+console.log(`Área do triângulo Escaleno ${tri3.calcArea()}`);
